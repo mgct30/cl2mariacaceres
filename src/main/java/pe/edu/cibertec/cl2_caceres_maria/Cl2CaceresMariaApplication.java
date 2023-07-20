07c02c571f3a8ab22165a6137db95b4749eed45c
mgct30 <mgct30@gmail.com>
2023-07-19 23:21:36 -0500

    se envian los datos finales
    se envian los datos finales

---
+++ src/main/java/pe/edu/cibertec/cl2_caceres_maria/Cl2CaceresMariaApplication.java
@@ -0,0 +1,14 @@
+package pe.edu.cibertec.cl2_caceres_maria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl2CaceresMariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl2CaceresMariaApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/pe/edu/cibertec/cl2_caceres_maria/Cl2CaceresMariaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.cl2_caceres_maria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2CaceresMariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
